f845274a29eed7bc09fe28e4e0550e880ae221a6
Satvik Sawhney <101198384+SawhneySatvik@users.noreply.github.com>
2024-01-28 15:05:40 +0530

    This is the connection.java file for JDBC.

---
+++ Conn.java
@@ -0,0 +1,34 @@
+package UniBuddy;
+
+import java.sql.*;
+
+public class Conn{
+
+    /*
+     * 1) Register the DRIVER CLASS
+     * 2) CREATING CONNECTION string
+     * 3) Creating statements
+     * 4) Executing MySQL Queries
+     * 5) CLOSING the CONNECTION
+     */
+
+    Connection c;
+    Statement s;
+    
+    Conn(){
+
+        try{
+            Class.forName("com.mysql.cj.jdbc.Driver"); 
+            /*Java Projects-> Referenced Libraries*/
+
+            c = DriverManager.getConnection("jdbc:mysql://localhost:3306/UniBuddy","root", "Rajni@1999");
+            /*Connection  */
+            
+            s = c.createStatement();
+        }
+        catch(Exception e){
+            e.printStackTrace();
+        }
+
+    }
+}
